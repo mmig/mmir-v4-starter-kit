9523b5ac339c9b1775896c29680c6e9a02a4fb9b
russa <aaron.russ@dfki.de>
2017-01-12 13:12:57 +0100

    integrating of MMIR wrappers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+import {RegistrationPage} from './../pages/registration-page/registration-page';
+
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 
@@ -7,12 +10,19 @@ import { LoginPage } from '../pages/login-page/login-page';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
+import { FabMiclevels } from './../components/fab-miclevels/fab-miclevels';
+
+import { MmirProvider } from '../providers/mmir';
+import { AppConfig } from './../providers/app-config';
+
 @NgModule({
   declarations: [
     MyApp,
     LoginPage,
+    RegistrationPage,
     Page1,
-    Page2
+    Page2,
+    FabMiclevels
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -21,9 +31,10 @@ import { Page2 } from '../pages/page2/page2';
   entryComponents: [
     MyApp,
     LoginPage,
+    RegistrationPage,
     Page1,
     Page2
   ],
-  providers: []
+  providers: [Storage, MmirProvider, AppConfig]
 })
 export class AppModule {}
