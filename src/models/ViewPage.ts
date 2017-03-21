1b44441a07fc012a9661775ccaf920cc5f80e82d
russa <aaron.russ@dfki.de>
2017-03-21 12:40:44 +0100

    added some mmir modules as (shortcut) properties

--- src/models/ViewPage.ts
+++ src/models/ViewPage.ts
@@ -1,4 +1,6 @@
-import {MmirModule} from './MmirInterfaces';
+import {IonicDialogManager} from './../providers/mmir';
+import {DialogManager} from './MmirInterfaces';
+import {MmirModule, LanguageManager, InputManager} from './MmirInterfaces';
 import {MmirProvider} from './../providers/mmir';
 
 export interface MmirAppModule extends MmirModule {
@@ -13,6 +15,43 @@ export class ViewPage {
 
   protected mmir: MmirAppModule;
 
+  protected _lang: LanguageManager;
+  protected _inp: InputManager;
+  protected _dlg: IonicDialogManager;
+
+  protected get lang(): LanguageManager {
+    if(!this._lang){
+      if(this.mmir && this.mmir.LanguageManager){
+        this._lang = this.mmir.LanguageManager;
+      } else {
+        return null;
+      }
+    }
+    return this._lang;
+  }
+
+  protected get inp(): InputManager {
+    if(!this._inp){
+      if(this.mmir && this.mmir.InputManager){
+        this._inp = this.mmir.InputManager;
+      } else {
+        return null;
+      }
+    }
+    return this._inp;
+  }
+
+  protected get dlg(): IonicDialogManager {
+    if(!this._dlg){
+      if(this.mmir && this.mmir.DialogManager){
+        this._dlg = this.mmir.DialogManager as IonicDialogManager;
+      } else {
+        return null;
+      }
+    }
+    return this._dlg;
+  }
+
   constructor(
     mmirProvider: MmirProvider
   ) {
@@ -23,13 +62,22 @@ export class ViewPage {
 
     this.mmir.app.triggerClickFeedback();
 
-		this.mmir.InputManager.raise('touch_input_event');
-		this.mmir.InputManager.raise('click_on_' + name, data);
+		// this.inp.raise('touch_input_event');
+		// this.inp.raise('click_on_' + name, data);
+    let emmaEvt = this.dlg._emma.toEmma(event, data);
+    this.dlg._emma._setEmmaFuncData(emmaEvt, 'understanding', {
+      name: name,
+      data: data
+    });
+    // this.dlg._emma.addTarget(emmaEvt, name, true);
+    // this.dlg._emma.addProperty(emmaEvt, 'data', data, true);
+    console.log(emmaEvt);
+    this.inp.raise('touch', emmaEvt);
   }
 
   public localize(res: string) : string {
-    if(this.mmir && this.mmir.LanguageManager){
-      return this.mmir.LanguageManager.getText(res);
+    if(this.lang){
+      return this.lang.getText(res);
     } else {
       console.warn('mmir.LanguageManager not ready yet...');
       return '';
